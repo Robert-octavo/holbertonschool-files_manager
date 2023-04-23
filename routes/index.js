a4b70d6efeb848c30c557f0dd7bea487fc45115f
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-04-22 23:26:24 -0500

    add a new endpoint - task 3

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@ all endpoints of our API:
   - GET /status => AppController.getStatus
   - GET /stats => AppController.getStats
 
+  - POST /users => UsersController.postNew
+
 */
 
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
