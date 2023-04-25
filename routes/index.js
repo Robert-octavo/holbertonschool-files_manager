32b063813d57e235c1afeefa2bf97169b7b799f2
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-04-24 21:42:22 -0500

    add new endpoint - Task 5

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,9 @@ all endpoints of our API:
   - GET /disconnect => AuthController.getDisconnect
   - GET /users/me => UserController.getMe
 
+add a new endpoint:
+
+  - POST /files => FilesController.postUpload
 
 */
 
@@ -20,6 +23,7 @@ const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 const UserController = require('../controllers/UserController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
@@ -29,5 +33,6 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
